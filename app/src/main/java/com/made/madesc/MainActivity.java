97f34875a9d53a411389d0b1b415d289f9871d40
Thyago F. da Silva <thferreira@gmail.com>
2018-05-18 17:18:50 -0300

    feat: Set login button handler

--- app/src/main/java/com/made/madesc/MainActivity.java
+++ app/src/main/java/com/made/madesc/MainActivity.java
@@ -1,7 +1,11 @@
 package com.made.madesc;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Sets login button listener
+        Button loginButton = (Button) findViewById(R.id.bt_login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Context context = MainActivity.this;
+                String textToShow = "Login Clicked!";
+                Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void onLoginButtonPress() {
+        Context context = MainActivity.this;
+        String textToShow = "Login Clicked!";
+        Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
     }
 }
